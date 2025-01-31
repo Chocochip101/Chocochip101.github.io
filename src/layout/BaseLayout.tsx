3eabd170f8b40fb642c895b7f1e3413204f1d607
Kwoun Ki Ho <fingercut3822@gmail.com>
2025-02-01 02:50:16 +0900

    Revert "feat: 서치 콘솔 메타 변경"
    This reverts commit 5978295175143e9a08038bd54d6ff0704db963a8.

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -79,10 +79,13 @@ const BaseLayout = ({path, children, meta}: BaseLayoutProps) => {
                     property="og:site_name"
                     content={meta ? meta.title : INITIAL_META.title}
                 />
+                <meta
+                    name="google-site-verification"
+                    content="AeVWzvBApSe4tJz0xn1wSTpJBQJb-QLALeMbZ3y9yV4"
+                />
             </Helmet>
             <Global styles={globalStyle}/>
             <Header path={path}/>
-            <meta name="google-site-verification" content="AeVWzvBApSe4tJz0xn1wSTpJBQJb-QLALeMbZ3y9yV4"/>
             <Body>{children}</Body>
             <Footer/>
         </ThemeProvider>
